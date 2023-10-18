167f8d5d1be744e84e344a2c0bdada986abd4556
Misael-GC <misael.gomez1623@gmail.com>
2023-10-17 21:32:49 -0600

    Nadvar base v1

--- src/Nadvar/index.js
+++ src/Nadvar/index.js
@@ -3,20 +3,23 @@ import './Nadvar.css';
 
 function Nadvar() {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className='Nav-Container'>
+      <nav className="navbar navbar-expand-lg bg-dark bg-dark border-bottom border-body" data-bs-theme="dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+      <img src="https://i.ibb.co/CPNtDVR/23bae78d-1953-4cd2-83ff-fb4c40400863-modified.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top mx-3"></img>
+      
+        <a className="navbar-brand" href="#">Optima</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+  
+        <div className="collapse navbar-collapse Nav-Options" id="navbarNav">
+          <ul className="navbar-nav ">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link 	" href="#">Features</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Pricing</a>
@@ -24,6 +27,18 @@ function Nadvar() {
             <li className="nav-item">
               <a className="nav-link disabled" aria-disabled="true">Disabled</a>
             </li>
+            {/* code */}
+            <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+            {/* code */}
           </ul>
         </div>
       </div>
