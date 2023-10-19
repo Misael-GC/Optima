8e24d3f7e670560d37ac93ea02f4ffec770588d1
Misael-GC <misael.gomez1623@gmail.com>
2023-10-19 02:11:55 -0600

    Detalles

--- src/Whatsapp/index.js
+++ src/Whatsapp/index.js
@@ -4,20 +4,11 @@ import './Whatsapp.css';
 function Whatsapp() {
   return (
     <div >
-        <button
-         className="CreateTodoButton" 
-        //  onClick={
-        //     (event) =>{
-        //         console.log('le diste click')
-        //         console.log(event)
-        //         console.log(event.target)
-        //     }
-        //  }
-         >
+        <div className="CreateTodoButton">
                 <a aria-label="Chat on WhatsApp" href="https://wa.me/2221189275" target='_blank'>
                     <i class="fa-brands fa-whatsapp icon-whatsApp"></i>
                 </a>
-         </button>
+         </div>
     </div>
   )
 }
