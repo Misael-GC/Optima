104459627ebb6167954ab64e44abbfb0cd84a81c
Misael-GC <misael.gomez1623@gmail.com>
2023-10-20 00:49:04 -0600

    mision-vision v1

--- src/SeccionCinco/index.js
+++ src/SeccionCinco/index.js
@@ -3,7 +3,29 @@ import './SeccionCinco.css';
 
 function SeccionCinco() {
   return (
-    <div>SeccionCinco</div>
+    <section className='Mision-Container bg-black'>
+      <div className='u-wrapper1'>
+        <div className='Mision-Container-secundario'>
+          {/* separador */}
+          <div class="card border-info mb-3 Card-principal bg-black" style={{width: "18rem"}}>
+              <div class="card-header bg-transparent border-info text-light">Misión</div>
+                <div class="card-body text-info">
+                  <h5 class="card-title Card-titulo">Success card title</h5>
+                  <p class="card-text Card-texto">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+          </div>
+          {/* separador */}
+          <div class="card border-info mb-3 Card-principal bg-black" style={{width: "18rem"}}>
+              <div class="card-header bg-transparent border-info text-light">Visión</div>
+                <div class="card-body text-info">
+                  <h5 class="card-title">Success card title</h5>
+                  <p class="card-text Card-texto">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+          </div>
+          {/* separador */}
+        </div>
+      </div>
+    </section>
   )
 }
 
