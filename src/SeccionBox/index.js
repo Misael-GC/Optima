8b58c876024de2641724ffb1f5e07ec30239f961
Misael-GC <misael.gomez1623@gmail.com>
2023-10-20 00:17:52 -0600

    sección de planes de pago

--- src/SeccionBox/index.js
+++ src/SeccionBox/index.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import './SeccionBox.css'
 
 function SeccionBox() {
   return (
-   <ul>
-    <li><h2>Seccion Box</h2></li>
-   </ul>
+   <section className='Plans'>
+      <div className='u-wrapper1'>
+        <div className='Plans-heading'>
+            <h2>Elige un plan</h2>
+            <p>
+                <span>Accede a nuestros cursos y contenidos exclusivos.</span> <br/>
+                Aprovecha los beneficios de la plataforma<br/>
+                Clases en vivo y grabados
+            </p>
+        </div>
+        <div className='00000000'>
+        <div className="LearningRoutes-slider-Plans Container-CardsPlans" aria-label="carousel">
+          {/* other */}
+            <div class="card Card-ContainerPlan" style={{width: "18rem;"}}>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        {/*  */}
+            <div class=" card Card-ContainerPlan" style={{width: "18rem;"}}>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        {/*  */}
+            <div class="card Card-ContainerPlan" style={{width: "18rem;"}}>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        {/*  */}
+            <div class="card Card-ContainerPlan" style={{width: "18rem;"}}>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        {/*  */}
+        </div>
+        </div>
+      </div>
+   </section>
   )
 }
 
